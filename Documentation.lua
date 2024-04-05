e77cc23ec3fe8f38d491c6198c98a4e5c9a32db5
Crvstal8100 <99895952+Crvstal8100@users.noreply.github.com>
2024-04-05 09:55:06 +0200

    Update Documentation.lua

--- Documentation.lua
+++ Documentation.lua
@@ -5,3 +5,8 @@ Rotate Camera = Hold Right MouseClick
 Q or Z = Down
 E or I = Up
 ]]--
+
+-- HOW TO CHECK IF FREECAM IS ENABLED:
+local Module = loadstring(game:HttpGet('https://raw.githubusercontent.com/Crvstal8100/Roblox-FreeCam/main/Main.lua'))()
+
+print(Module:IsEnabled())
